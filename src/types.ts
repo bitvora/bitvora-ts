a2b8aa96f01d941bf9e7431910bf8301712b2ec3
Barry Deen <barrydeen@protonmail.com>
2024-07-08 15:40:17 -0400

    Define Withdrawals and Deposits

--- src/types.ts
+++ src/types.ts
@@ -2,35 +2,107 @@ export interface Metadata {
   [key: string]: string;
 }
 
-export interface DepositChainConfirmedPayload {
+export interface DepositOnChainPayload {
   deposit_id: string;
   chain_txid: string;
   chain_deposit_address: string;
   amount_sats: number;
   fee: number;
+  status: "pending" | "settled" | "failed";
   metadata: Metadata;
 }
 
-// Define other event payload types here
-export interface AnotherEventPayload {
-  // Define the payload structure
+export interface WithdrawalOnChainPayload {
+  withdrawal_id: string;
+  chain_txid: string;
+  chain_withdrawal_address: string;
+  amount_sats: number;
+  fee: number;
+  status: "pending" | "settled" | "failed";
+  metadata: Metadata;
+}
+
+export interface WithdrawalLightningPayload {
+  withdrawal_id: string;
+  recipient: string;
+  status: "pending" | "settled" | "failed";
+  amount_sats: number;
+  payment_route: {
+    hops: Array<{
+      amt_to: string;
+      expiry: number;
+      chan_id: string;
+      chan_capacity: string;
+      amt_to_forward: string;
+    }>;
+  };
+  payment_preimage: string;
+  metadata: Metadata;
+}
+
+export interface DepositLightningPayload {
+  deposit_id: string;
+  sender: string;
+  status: "pending" | "settled" | "failed";
+  amount_sats: number;
+  payment_request: string;
+  description_hash: string;
+  expiry_seconds: number;
+  payment_preimage: string;
+  r_hash: string;
+  metadata: Metadata;
 }
 
 export interface WebhookPayloads {
-  deposit_chain_confirmed: DepositChainConfirmedPayload;
-  another_event: AnotherEventPayload;
+  deposit_chain_pending: DepositOnChainPayload;
+  deposit_chain_completed: DepositOnChainPayload;
+  withdrawal_chain_pending: WithdrawalOnChainPayload;
+  withdrawal_chain_completed: WithdrawalOnChainPayload;
+  withdrawal_lightning_pending: WithdrawalLightningPayload;
+  withdrawal_lightning_completed: WithdrawalLightningPayload;
+  deposit_lightning_completed: DepositLightningPayload;
 }
 
-export interface DepositChainConfirmedEvent {
-  event_type: "deposit_chain_confirmed";
-  data: DepositChainConfirmedPayload;
+export interface DepositOnChainCompletedEvent {
+  event_type: "deposit.onchain.completed";
+  data: DepositOnChainPayload;
 }
 
-export interface AnotherEvent {
-  event_type: "another_event";
-  data: AnotherEventPayload;
+export interface DepositOnChainPendingEvent {
+  event_type: "deposit.onchain.pending";
+  data: DepositOnChainPayload;
 }
 
-// Define other event types here
+export interface DepositLightningCompletedEvent {
+  event_type: "deposit.lightning.completed";
+  data: DepositLightningPayload;
+}
 
-export type WebhookEvent = DepositChainConfirmedEvent | AnotherEvent;
+export interface WithdrawalChainPendingEvent {
+  event_type: "withdrawal.chain.pending";
+  data: WithdrawalOnChainPayload;
+}
+
+export interface WithdrawalChainCompletedEvent {
+  event_type: "withdrawal.chain.completed";
+  data: WithdrawalOnChainPayload;
+}
+
+export interface WithdrawalLightningPendingEvent {
+  event_type: "withdrawal.lightning.pending";
+  data: WithdrawalLightningPayload;
+}
+
+export interface WithdrawalLightningCompletedEvent {
+  event_type: "withdrawal.lightning.completed";
+  data: WithdrawalLightningPayload;
+}
+
+export type WebhookEvent =
+  | DepositOnChainCompletedEvent
+  | DepositOnChainPendingEvent
+  | DepositLightningCompletedEvent
+  | WithdrawalChainPendingEvent
+  | WithdrawalChainCompletedEvent
+  | WithdrawalLightningPendingEvent
+  | WithdrawalLightningCompletedEvent;
